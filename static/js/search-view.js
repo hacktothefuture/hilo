e978390be619e1b8d3b8e63e8818a0820eb90e01
Mitchell Sternke <mitchellsternke@gmail.com>
2014-09-27 09:22:15 -0500

    Linked the index to the product page

--- static/js/search-view.js
+++ static/js/search-view.js
@@ -2,15 +2,20 @@ function onSearchSuccess( response ) {
     for( var i = 0; i < response.CatalogEntryView.length; i++ ) {
         var item = response.CatalogEntryView[i];
         
-        $("#search-results-table > tbody:last").append("<tr><td><img width='100' height='100' src='" + item.fullImage + "' />" + "</td><td>" + item.title + "</td></tr>");
+        $("#search-results-table > tbody:last").append("<tr><td><img width='100' height='100' src='" + item.fullImage + "' />" + "</td><td><a href='/products/" + item.partNumber + "'>" + item.title + "</a></td></tr>");
     }
+    
+    $("#search-results-table").fadeIn();
 }
 
 function onSearchError( response ) {
+    $("#search-results-table").fadeIn();
 }
 
 function executeSearch() {
     $("#search-results-table > tbody").empty();
+    
+    $("#search-results-table").fadeOut();
     
     $.ajax({
         url : "https://api.target.com/v2/products/search?searchTerm=" +
